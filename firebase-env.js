45186688b5d2af02c714c76ce131365a7057aac7
Hridesh Kapur <hrideshkapur@gmail.com>
2026-07-09 17:10:16 -0400

    Include firebase-env.js for GitHub Pages hosting

---
+++ firebase-env.js
@@ -0,0 +1,9 @@
+// This file is gitignored so your keys don't end up on GitHub
+window.firebaseConfig = {
+  apiKey: "AIzaSyAbx5wIroZrUrNmJWANF97Ec8UCnzLM3rE",
+  authDomain: "the-viking-project-f653a.firebaseapp.com",
+  projectId: "the-viking-project-f653a",
+  storageBucket: "the-viking-project-f653a.firebasestorage.app",
+  messagingSenderId: "809233063243",
+  appId: "1:809233063243:web:5eca2061d55a416c87dcce"
+};
